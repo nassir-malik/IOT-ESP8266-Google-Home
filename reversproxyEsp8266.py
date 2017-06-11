6cf874e6f7d01c780185982a5dc6fc611561b706
nassiramalik <nassir.malik@gmail.com>
2017-06-11 18:12:42 -0500

    new

---
+++ reversproxyEsp8266.py
@@ -0,0 +1,61 @@
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+# noinspection PyUnresolvedReferences
+import threading
+from flask import Flask
+from flask import request
+import json, os
+from flask import make_response
+app = Flask(__name__)
+
+clients = []
+address1 = '127.0.0.1'
+class SimpleChat(WebSocket):
+
+    def handleMessage(self):
+        address1 = self.address
+        for client in clients:
+          if client != self:
+             client.sendMessage(self.address[0] + u' - ' + self.data)
+
+    def handleConnected(self):
+       print(self.address, 'connected')
+       #address1 = self.address
+       for client in clients:
+          client.sendMessage(self.address[0] + u' - connected')
+       clients.append(self)
+
+    def handleClose(self):
+       clients.remove(self)
+       print(self.address, 'closed')
+       for client in clients:
+          client.sendMessage(self.address[0] + u' - disconnected')
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    #mkrsp= makeWebhookResult(req)
+    #r = make_response(json.dumps(mkrsp, indent=4))
+    #r.headers['Content-Type'] = 'application/json'
+    clients[0].sendMessage(address1[0] + u' - ' + str(req))
+    return "test"
+
+def flaskServer(address1):
+    port = int(os.getenv('PORT', 80))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='', threaded=True)
+
+def webSocketServer():
+    server = SimpleWebSocketServer('', 8000, SimpleChat)
+    server.serveforever()
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=flaskServer, args=(address1,))
+    t2 = threading.Thread(target=webSocketServer)
+    t1.start()
+    t2.start()
+
