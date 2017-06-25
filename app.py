9f840e12c7ced60b0ad7590d25eef9a0d65d0841
nassiramalik <nassir.malik@gmail.com>
2017-06-25 10:31:29 -0500

    new

--- app.py
+++ app.py
@@ -1,61 +1,102 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
-# noinspection PyUnresolvedReferences
+import websockets
+import asyncio
+import json
+import time
+# import logging
 import threading
-from flask import Flask
-from flask import request
-import json, os
-from flask import make_response
-app = Flask(__name__)
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# ws_logger = logging.getLogger('websockets.server')
+# ws_logger.setLevel(logging.INFO)
 
-clients = []
-address1 = '127.0.0.1'
-class SimpleChat(WebSocket):
+# logger = logging.getLogger(__name__)
 
-    def handleMessage(self):
-        address1 = self.address
-        for client in clients:
-          if client != self:
-             client.sendMessage(self.address[0] + u' - ' + self.data)
+class HttpWSSProtocol(websockets.WebSocketServerProtocol):
+    rwebsocket = None
+    rddata = None
+    async def handler(self):
+        try:
+            request_line, headers = await websockets.http.read_message(self.reader)
+            method, path, version = request_line[:-2].decode().split(None, 2)
 
-    def handleConnected(self):
-       print(self.address, 'connected')
-       #address1 = self.address
-       for client in clients:
-          client.sendMessage(self.address[0] + u' - connected')
-       clients.append(self)
+        except Exception as e:
+            print(e.args)
+            self.writer.close()
+            self.ws_server.unregister(self)
 
-    def handleClose(self):
-       clients.remove(self)
-       print(self.address, 'closed')
-       for client in clients:
-          client.sendMessage(self.address[0] + u' - disconnected')
+            raise
+
+        # TODO: Check headers etc. to see if we are to upgrade to WS.
+        if path == '/ws':
+            # HACK: Put the read data back, to continue with normal WS handling.
+            self.reader.feed_data(bytes(request_line))
+            self.reader.feed_data(headers.as_bytes().replace(b'\n', b'\r\n'))
+
+            return await super(HttpWSSProtocol, self).handler()
+        else:
+            try:
+                return await self.http_handler(method, path, version)
+            except Exception as e:
+                print(e)
+            finally:
+
+                self.writer.close()
+                self.ws_server.unregister(self)
 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
+    async def http_handler(self, method, path, version):
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+        try:
+            await self.rwebsocket.send(json.dumps(self.reader._buffer.decode('utf-8')))
+            print('In http function:')
+            self.rddata = await self.rwebsocket.recv()
+            print('Out HTTP function:')
+            print("HTTP function")
 
-    #mkrsp= makeWebhookResult(req)
-    #r = make_response(json.dumps(mkrsp, indent=4))
-    #r.headers['Content-Type'] = 'application/json'
-    clients[0].sendMessage(address1[0] + u' - ' + str(req))
-    return "test"
+            response = '\r\n'.join([
+                'HTTP/1.1 200 OK',
+                'Content-Type: text/html',
+                '',
+                '<html><body><h1>'+self.rddata+'</h1></body></html>',
+            ])
+        except Exception as e:
+            print(e)
+        self.writer.write(response.encode())
 
-def flaskServer(address1):
-    port = int(os.getenv('PORT', 80))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='', threaded=True)
+def updateData(data):
+    HttpWSSProtocol.rddata = data
 
-def webSocketServer():
-    server = SimpleWebSocketServer('', 8000, SimpleChat)
-    server.serveforever()
+async def ws_handler(websocket, path):
+    #logger.info('Client connecting: %s, %s', websocket, path)
+    game_name = 'g1'
 
-if __name__ == '__main__':
-    t1 = threading.Thread(target=flaskServer, args=(address1,))
-    t2 = threading.Thread(target=webSocketServer)
-    t1.start()
-    t2.start()
+    try:
+        #g, player_name = join_game(game_name, websocket)
+        HttpWSSProtocol.rwebsocket = websocket
+        await websocket.send(
+            json.dumps({'event': 'init', 'player_name': "test", 'score': ['score']})
+        )
+        data ='{"empty":"empty"}'
+        while True:
+        #     #data = await websocket.recv()
+            print('In ws function:')
+            data = await websocket.recv()
+            print('Out ws function:')
+        #
+        #logger.info('data: %s', data)
+        #     data = json.loads(data)
+            updateData(data)
+        print('From sock func:' + str(data))
 
+            #handle_event(g, player_name, data, websocket)
+
+    finally:
+        #logger.info(u'Disconnecting client and leaving game')
+        #leave_game(websocket)
+        print("")
+
+
+start_server = websockets.serve(ws_handler, '0.0.0.0', 80, klass=HttpWSSProtocol)
+# logger.info('Listening on port %d', port)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
